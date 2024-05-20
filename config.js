37b6dbdb55a98ced028e6cd7ba1e4ddd27660472
Bruno Lattanzio <brunoj.lattanzio@gmail.com>
2024-05-20 20:06:18 -0300

    fixing config error

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ const {
   const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
   const clurl = MONGO_URL
   const rwurl = MONGO_URL_RW
+  const port = PORT
 
   module.exports = {
     url,
