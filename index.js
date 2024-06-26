e574e880e6974c1dd85907c15823d818f8bf8850
Bruno Lattanzio <brunoj.lattanzio@gmail.com>
2024-06-26 19:40:46 -0300

    fixing cors

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const corsOptions = require('./cors-options')
-const {mongoose} = require('mongoose');
+const corsOptions = require('./cors-options');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const {url, rwurl, port} = require('./config')
-const {specs} = require('./swagger_config');
+const { url, rwurl, port } = require('./config');
+const { specs } = require('./swagger_config');
 const swaggerUi = require("swagger-ui-express");
-const {AuthRouter} = require('./routes/authRoutes')
+const { AuthRouter } = require('./routes/authRoutes');
 const setupDatabase = require('./setup');
-const User = require('./model/user_model'); 
+const User = require('./model/user_model');
 
-const routes = new AuthRouter();
-
-//database connection
-mongoose.connect(rwurl) //Web: MONGO_URL //docker: url
-.then(()=> console.log('Database Connected'))
-.catch((err) => console.log('Database not Connected', rwurl, err))
-
-
+// database connection
+mongoose.connect(rwurl)
+  .then(() => console.log('Database Connected'))
+  .catch((err) => console.log('Database not Connected', rwurl, err));
 
 const app = express();
-//middleware
-app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
-app.use('/', routes.get_routes())
+
+// middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use('/', new AuthRouter().get_routes());
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(specs, { explorer: true })
 );
-app.use(cors(corsOptions));
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-//sonar tryout
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -93,25 +93,16 @@ class AuthRouter {
     constructor(){
         this.auth_router = express.Router();
         this.auth_controller = new AuthController();
-        this.auth_router.use(
-            cors({
-                credentials: true,
-                origin: 'http://localhost:8090'
-            })
-        )
-        this.auth_router.post('/register', (req, res) => this.auth_controller.registerUser(req, res))
-        this.auth_router.post('/login', (req, res) => this.auth_controller.loginUser(req, res))
-        this.auth_router.get('/profile', (req, res) => this.auth_controller.getProfile(req, res))
-        this.auth_router.delete('/delete', (req, res) => this.auth_controller.deleteUser(req, res))
+        // Remove the specific CORS middleware here
+        this.auth_router.post('/register', (req, res) => this.auth_controller.registerUser(req, res));
+        this.auth_router.post('/login', (req, res) => this.auth_controller.loginUser(req, res));
+        this.auth_router.get('/profile', (req, res) => this.auth_controller.getProfile(req, res));
+        this.auth_router.delete('/delete', (req, res) => this.auth_controller.deleteUser(req, res));
     }
 
     get_routes() {
-        return this.auth_router
+        return this.auth_router;
     }
 }
 
-
-
-
-
-module.exports = {AuthRouter}
+module.exports = { AuthRouter };
