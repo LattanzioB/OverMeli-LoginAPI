a7ebcd3b8483190bd95e086579bac19730df9304
Bruno Lattanzio <brunoj.lattanzio@gmail.com>
2024-07-04 16:41:31 -0300

    mongo cloud url added to env

--- src/config.js
+++ src/config.js
@@ -6,11 +6,12 @@ const {
     DB_NAME,
     MONGO_URL_RW,
     MONGO_URL,
+    MONGO_CURL,
     PORT
   } = process.env;
   
   const url = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
-  const clurl = "mongodb+srv://taatolat:puL3LCO7E0kAconR@overmeli.qyyqrk9.mongodb.net/?retryWrites=true&w=majority&appName=OverMeLi"
+  const clurl = MONGO_CURL
   
   const rwurl = MONGO_URL_RW
   const port = PORT || 8000
